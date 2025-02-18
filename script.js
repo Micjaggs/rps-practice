d12fedfe2e5cbd4e801594fd1c874d1c5df10446
Mic <126879111+Micjaggs@users.noreply.github.com>
2025-02-18 16:19:44 +1000

    linked all elements to the DOM and re-factored game logic

--- script.js
+++ script.js
@@ -3,45 +3,14 @@
 const rockButton = document.getElementById("rock")
 const paperButton = document.getElementById("paper")
 const scissorsButton = document.getElementById("scissors")
+//Selection display divs
 const playerSelection = document.getElementById("player-selection")
-
-function getComputerChoice() {
-    let compChoice = Math.floor(Math.random() * 3)
-    if (compChoice == 0) {
-        return "rock"
-    }
-    else if ( compChoice == 1) {
-       return "paper"
-    }
-    else {
-        return "scissors"
-    }
-}
-
-// Event listeners that handle clicks
-rockButton.addEventListener('click', function() {
-    playRound("rock", getComputerChoice())
-
-})
-
-paperButton.addEventListener('click', function() {
-    playRound("paper", getComputerChoice())
-
-})
-
-scissorsButton.addEventListener('click', function() {
-    playRound("scissors", getComputerChoice())
-})
-
-
-
-
-// Function that gets the player choice
-
-function getHumanChoice() {
-    let humanChoice = prompt("What is your choice?")
-        return humanChoice.toLowerCase()
-    }
+const compSelection = document.getElementById("computer-selection")
+//Result declarations
+const result = document.getElementById("result")
+//Score Keeping and end game
+const score = document.getElementById("score")
+const endGame = document.getElementById("end-game")
 
 //Variables required to keep track of the game
 
@@ -50,50 +19,84 @@ let computerScore = 0
 let roundsPlayed = 1
 
 
+//Function that gets computer choice
+
+function getComputerChoice() {
+    let compChoice = Math.floor(Math.random() * 3)
+    if (compChoice == 0) {
+        compSelection.textContent = "Computer has chosen Rock"
+        return "rock"
+    }
+    else if ( compChoice == 1) {
+        compSelection.textContent = "Computer has chosen Paper"
+       return "paper"
+    }
+    else {
+        compSelection.textContent = "Computer has chosen Scissors"
+        return "scissors"
+}
+}
+
 //Function that generates the logic to play one round
 
 function playRound(humanSelection, computerSelection) {
     if ((humanSelection === "rock" && computerSelection === "scissors") || 
         (humanSelection === "scissors" && computerSelection === "paper") ||
         (humanSelection === "paper" && computerSelection ==="rock")) {
-            console.log(`Player wins with ${humanSelection}, computer loses with ${computerSelection}`)
-            humanScore++       
+            result.innerText = `Player wins with ${humanSelection}, computer loses with ${computerSelection}`
+            humanScore++
     }
     else if (humanSelection === computerSelection) {
-        console.log(`It's a tie, player and computer both selected ${humanSelection}`)
+        result.innerText = `It's a tie, player and computer both selected ${humanSelection}`  
     }
     else {
-        console.log(`Computer wins with ${computerSelection}, player loses with ${humanSelection}`)
+        result.innerText = `Computer wins with ${computerSelection}, player loses with ${humanSelection}`
         computerScore++
         
 }
-console.log(`rounds played ${roundsPlayed}, player score: ${humanScore}, computer score ${computerScore}`)
+    score.innerText = `Rounds played ${roundsPlayed}, Player score: ${humanScore}, Computer score: ${computerScore}`
+    endGame.innerText = ""
 }
 
-//Function that plays the game with 5 rounds
+//Function to check if the game is over
 
-// function playGame() {
-//     if (roundsPlayed < 6){
-//         let humanSelection = getHumanChoice()
-//         let computerSelection = getComputerChoice()
-//         if (humanSelection !== "rock" && humanSelection !== "paper" && humanSelection !== "scissors" || humanSelection == "")
-//             console.log(`Please type rock, paper, or scissors, ${humanSelection} is not an option`)
-//         else {
-//             playRound(humanSelection, computerSelection);
-//             roundsPlayed++;
-//         }       
-//     }
-//     else {
-//         console.log("Game Over")
-//         humanScore = 0
-//         computerScore = 0
-//         roundsPlayed = 0
-//     }
-    
-//     playGame()
-//}
+function checkEndGame() {
+    if (humanScore > computerScore) {
+        endGame.innerText = "Player wins! Game Over"
+        humanScore = 0
+        computerScore = 0
+        roundsPlayed = 1
+    }
+    else {
+        endGame.innerText = "Computer wins! Game Over"
+        humanScore = 0
+        computerScore = 0
+        roundsPlayed = 1
+    }
+}
 
-//Calling the play game function to initiate the game
+// Event listeners that initiate game and handle clicks
 
-//playGame()
-
+rockButton.addEventListener('click', function() {
+    if (roundsPlayed < 6) {
+    playRound("rock", getComputerChoice())
+    playerSelection.textContent = "Player has chosen Rock"
+    roundsPlayed++
+    } else checkEndGame()
+})  
+paperButton.addEventListener('click', function() {
+    if (roundsPlayed < 6) {
+    playRound("paper", getComputerChoice())
+    playerSelection.textContent = "Player has chosen Paper"
+    roundsPlayed++
+    }
+    else checkEndGame()
+})
+scissorsButton.addEventListener('click', function() {
+    if (roundsPlayed < 6) {
+    playRound("scissors", getComputerChoice())
+    playerSelection.textContent = "Player has chosen Scissors"
+    roundsPlayed++
+    }
+    else checkEndGame()
+})
